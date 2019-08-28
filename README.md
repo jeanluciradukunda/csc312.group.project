47267c63e7e5e251f367617863e97ef3f77cce51
ZiyaadLakay <ZiyaadLakay@users.noreply.github.com>
2019-08-28 11:24:45 +0200

    minor changes

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # csc312.group.project
+
+TEMPLATES - This is where we store our HTML Files
+STATIC - This is where we do our CSS
+WEBAPP - This is where we do our methods in pyhton
+
+WEBAPP :
+-models; Using Classes to store information
+-views ; Defining our methods and sending it to HTML
--- dvdstore/webapp/views.py
+++ dvdstore/webapp/views.py
@@ -7,21 +7,25 @@ def home(request):
     dvd1 = DVD()
     dvd1.name = 'John Wick the second coming'
     dvd1.desc = 'A movie where Keanu Reeves Dog dies, and he kills everyone in the movie'
+    dvd1.img = 'john_wick.jpg'
     dvd1.inStock = True
 
     dvd2 = DVD()
     dvd2.name = 'Shawshank Redmption'
     dvd2.desc = 'A movie about rape in prison in the ass'
+    dvd2.img = 'shawshank.jpg'
     dvd2.inStock = True
 
     dvd3 = DVD()
-    dvd3.name = 'Lion King 2'
+    dvd3.name = 'Lion King'
     dvd3.desc = 'A movie about Lions coming of age'
+    dvd3.img = 'lion_king.jpg'
     dvd3.inStock = False
 
     dvd4 = DVD()
     dvd4.name = 'Dave Chapelle'
     dvd4.desc = 'A movie about Lions coming of age'
+    dvd4.img = 'chappelle.jpg'
     dvd4.inStock = False
     
     dvds = [dvd1,dvd2,dvd3, dvd4]

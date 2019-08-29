9c923e8028533fa8f50098eb21bb7cb5467bf5dd
ZiyaadLakay <ZiyaadLakay@users.noreply.github.com>
2019-08-29 18:47:51 +0200

    added search function

--- dvdstore/webapp/views.py
+++ dvdstore/webapp/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
 from .models import DVD
-# Create your views here.
+from django.core.paginator import EmptyPage,PageNotAnInteger, Paginator
+from django.db.models import Q
 
+
+#This is the homepage for the User
 def home(request):
 
     dvds = DVD.objects.all() #imports dvds from database
+    
+    query = request.GET.get("query")
+    if query:
+        dvds = DVD.objects.filter(Q(name__icontains=query))
 
-    return render(request, 'home.html', {'dvds':dvds})
+    paginator = Paginator(dvds, 3) # Show 3 dvds per page
+    page = request.GET.get('page')
+    dvds = paginator.get_page(page)  
+
+    return render(request, 'home.html', {'dvds':dvds}) #renders the page
+
+#This is the page for clerks
+def clerk(request):
+    dvds = DVD.objects.all() #imports dvds from database
+
+    query = request.GET.get("query")
+    if query:
+        dvds = DVD.objects.filter(Q(name__icontains=query))
+
+    paginator = Paginator(dvds, 3) # Show 3 dvds per page
+    page = request.GET.get('page')
+    dvds = paginator.get_page(page)  
+    
+    return render(request, 'clerk.html', {'dvds':dvds})
+

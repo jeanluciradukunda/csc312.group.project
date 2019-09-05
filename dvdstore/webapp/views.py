c64b630a742f5d6713de9a1ac10a238b33989fa2
ZiyaadLakay <ZiyaadLakay@users.noreply.github.com>
2019-09-05 11:30:41 +0200

    change to clerk rights

--- dvdstore/webapp/views.py
+++ dvdstore/webapp/views.py
@@ -11,14 +11,19 @@ def home(request):
     dvds = DVD.objects.all() #imports dvds from database
     
     query = request.GET.get("query")
-    if query:
-        dvds = DVD.objects.filter(Q(Title__icontains=query))#Search Function according to name
+    if query: 
+        if  Q(Title__icontains=query) :
+            dvds = DVD.objects.filter(Q(Title__icontains=query))#Search Function according to name
+        elif Q(genre__icontains=query):
+            dvds = DVD.objects.filter(Q(genre__icontains=query))#Search Function according to name
 
     paginator = Paginator(dvds, 3) # Show 3 dvds per page
     page = request.GET.get('page')
     dvds = paginator.get_page(page)  
+    
+    genre = {'Action', 'Comedy', 'Drama', 'Family', 'Romance'}
 
-    return render(request, 'home.html', {'dvds':dvds}) #renders the page
+    return render(request, 'home.html', {'dvds':dvds}, {'genre':genre}) #renders the page
 
 #This is the page for clerks
 def clerk(request):

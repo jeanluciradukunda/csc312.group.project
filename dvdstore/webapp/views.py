c47cedc53c9934704aaf2973af08bb0f7d4b49b8
uzairjones <uzairjoneswolf@gmail.com>
2019-09-07 00:05:04 +0200

    add movie and users for clerk

--- dvdstore/webapp/views.py
+++ dvdstore/webapp/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from .models import DVD
 from django.core.paginator import EmptyPage,PageNotAnInteger, Paginator
 from django.db.models import Q
-
-
+from django.contrib.auth.models import User, auth
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.core.files.storage import FileSystemStorage
+from .form import DocumentForm
 #This is the homepage for the User
     
 def home(request):
@@ -36,6 +39,35 @@ def clerk(request):
     paginator = Paginator(dvds, 3) # Show 3 dvds per page
     page = request.GET.get('page')
     dvds = paginator.get_page(page)  
-    
-    return render(request, 'clerk.html', {'dvds':dvds})
+    form=DocumentForm()
+    context_dict = { 'dvds':dvds ,'form': form}
+    return render(request, 'clerk.html',context_dict)
 
+def register2(request):
+
+    if request.method == 'POST':
+        first_name= request.POST['first_name']
+        last_name= request.POST['last_name']
+        username= request.POST['username']
+        email= request.POST['email']
+        password1= first_name[0]+last_name
+        
+
+        if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('clerk')
+        elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+        user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
+        user.save()
+        messages.info(request, 'User Created')
+
+    return redirect('/clerk')
+
+def model_form_upload(request):
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+        
+    return redirect('/clerk')
